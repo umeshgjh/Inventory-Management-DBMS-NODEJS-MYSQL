1fc87388b313c01465009bac156039dbc4a9a133
UMESH GJH <gjhumesh@gmail.com>
2024-04-26 14:31:17 +0530

    scroll button working now

---
+++ frontend/src/useScrollPosition.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+const useScrollPosition = () => {
+  const [showFooter, setShowFooter] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollHeight = document.documentElement.scrollHeight;
+      const scrollTop = document.documentElement.scrollTop;
+      const clientHeight = document.documentElement.clientHeight;
+
+      if (scrollTop + clientHeight >= scrollHeight) {
+        setShowFooter(true);
+      } else {
+        setShowFooter(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return showFooter;
+};
+
+export default useScrollPosition;
